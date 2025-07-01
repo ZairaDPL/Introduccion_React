a77da7ca22a9e8bc3af1b27e100da455a47039ec
zaira_daniela_pacheco_limon <zairadpl18@gmail.com>
2025-06-30 21:32:43 -0600

    📝 Cambio en los componentes

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,6 +1,9 @@
+// Archivos de punto de entrada para una aplicación React
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+// Importación de los estilos 
 import './index.css'
+// Importación del componente principal de la App
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
